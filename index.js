b28c64bf82aeb972aff9fe8cd579431a18ac8e99
Madineto <uokeyezenekwe@yahoo.com>
2024-05-16 10:36:12 +0100

    backendwork done

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+
+const server = http.createServer((req, res) => {
+  
+
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  
+  
+  res.end('Backend WeekOne\n');
+});
+
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000/');
+});
